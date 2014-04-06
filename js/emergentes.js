342a0a0d8c706ad3616e0432faf8b812efe99e91
ConectaOaxaca <conectaoax@gmail.com>
2014-04-06 15:30:31 -0500

    JS y Moviles
    JS y Moviles

---
+++ js/emergentes.js
@@ -0,0 +1,10 @@
+ function despliegaModal(valor)
+  {
+    document.getElementById("ventanamodal").style.visibility=valor;
+  } 
+
+
+ function despliegaModalb(valor)
+  {
+    document.getElementById("despliegaModalLetraB").style.visibility=valor;
+  } 
